0d5c3f424b6400d216df6701aace44973f32391d
Bweble <afflexux@gmail.com>
2019-12-24 16:31:18 +0000

    done

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -3,12 +3,12 @@ let year = 2019;
 
 console.log(age, year);
 
-// age = 30;
-// console.log(age);
+age = 30;
+console.log(age);
 
-// const points = 100;
-// points = 50;
-// console.log(points);
+let points = 100;
+points = 50;
+console.log(points);
 
-// var score = 75;
-// console.log(score);
+var score = 75;
+console.log(score);
